afc36117fdae1ba1cebff7456dce4fd382ead069
danis <yinyy_danis@163.com>
2014-12-19 09:42:33 +0800

    更新了DicHandler

--- Common.BPM.Admin/sys/ashx/DicHandler.ashx.cs
+++ Common.BPM.Admin/sys/ashx/DicHandler.ashx.cs
@@ -101,6 +101,10 @@ namespace BPM.Admin.sys.ashx
                     context.Response.Write(new JsonMessage { Success = k==1, Data = k.ToString(), Message = msg}.ToString());
 
                     break;
+                case "code":
+                    var category = DicCategoryDal.Instance.GetWhere(new { Code=rpm.Request("code")}).FirstOrDefault();
+                    context.Response.Write(DicBll.Instance.GetDicListBy(category == null ? 0 : category.KeyId));
+                    break;
                 default: //字典列表
                     var categoryId = PublicMethod.GetInt(rpm.Request("categoryId"));
                     string dicJson = DicBll.Instance.GetDicListBy(categoryId);
