20dfa2236cec4b17635b8d1d166abab8d8dc0b95
尹元元 <yinyy_danis@163.com>
2016-10-02 21:24:14 +0800

    使用南京设备商提供的读卡器

--- sprinkler/sprinkler.ino
+++ sprinkler/sprinkler.ino
@@ -5,9 +5,9 @@ PLC							m		n
 5815
 */
 
+#include "CardReaderHelper2.h"
 #include "SimHelper.h"
 #include "PlcHelper.h"
-#include "CardReaderHelper.h"
 
 //信号质量的最小值
 #define SINGAL_QUALITY					5
@@ -31,12 +31,13 @@ typedef struct Info_Struct{
 	long time;
 };
 
-CardReaderHelperClass crh;
+CardReaderHelper2Class crh;
 PlcHelperClass plch;
 SimHelperClass simh;
 
 Info_Struct current, last;
 String shortMessage = "";
+String cards;
 
 void openIP() {
 	//打开IP应用
@@ -99,7 +100,7 @@ void setup()
 	last.driver.card = "";
 	last.trunk.card = "";
 	last.time = 0;
-
+	
 	//Arduino准备
 	//Serial.begin(9600);
 	Serial1.begin(DEFAULT_BAUD_SIM);//SIM900A
@@ -153,87 +154,126 @@ void setup()
 	//Serial.println("SIM900A IP Context 1 Ready.");
 }
 
-/*
-	完整流程：
-	1、读车卡。读不到，返回步骤1。
-	2、读人卡。读不到，返回步骤1。
-	3、询问PLC是否准备好——接收PLC回复的数据。
-	（1）如果接收数据错误（收到PLC返回的错误提示信息），则返回步骤3。
-	（2）如果接受数据正确，则：
-	——如果忙，则返回步骤3。
-	——如果PLC空闲，向发送车牌号、人员编号、车辆体积*10。
-	4、等待接收PLC完成加水后结束标志——接收到结束标志后通过SIM900A将设备编号、车牌号、人员编号、加水体积、浓度、类型发送到服务器。
-	5、返回步骤1
-	*/
 
-////新读卡器
+////老读卡器（玖锐）
 //void loop()
 //{
-//	delay(100);
-//	if (crh.getCard(&current.trunk.card, &current.driver.card)){
-//		current.time = millis();
+//	/*
+//	完整流程：
+//	1、读车卡。读不到，返回步骤1。
+//	2、读人卡。读不到，返回步骤1。
+//	3、询问PLC是否准备好——接收PLC回复的数据。
+//	（1）如果接收数据错误（收到PLC返回的错误提示信息），则返回步骤3。
+//	（2）如果接受数据正确，则：
+//	——如果忙，则返回步骤3。
+//	——如果PLC空闲，向发送车牌号、人员编号、车辆体积*10。
+//	4、等待接收PLC完成加水后结束标志——接收到结束标志后通过SIM900A将设备编号、车牌号、人员编号、加水体积、浓度、类型发送到服务器。
+//	5、返回步骤1
+//	*/
 //
-//		//车卡和人卡都读出来了，把车牌号和人员编号发送给PLC
-//		current.trunk.code = crh.getPlate(current.trunk.card);
-//		current.driver.code = crh.getCode(current.driver.card);
-//		float volumn = crh.getVolumn(current.trunk.card);
+//	shortMessage = "";
 //
-//		//只有满足一下条件才能继续操作
-//		//1、读到的卡不一样
-//		//2、读到是特殊卡
-//		//3、同一张卡，超过了5分钟
-//		if (
-//			(current.driver.code + current.trunk.code == "0000E00000") ||
-//			(current.driver.code + current.trunk.code != last.driver.code + last.trunk.code) ||
-//			(current.time - last.time >= 300000)){
+//	//delay(20);
+//	current.trunk.card = crh.readTrunkCard();//"544B000A4538323736320058";
+//	if (current.trunk.card != ""){
+//		int read_count = 0;
 //
-//			while (!plch.isReady()){
-//				delay(1000);//在PLC没有准备好的情况下，每1秒询问一次
-//				//Serial.println("PLC ready?");
-//			}
+//		do{
+//			//delay(20);
 //
-//			//Serial.println("PLC Ready");
-//			//PLC就绪
-//			plch.send(current.driver.code, current.trunk.code, volumn);
+//			current.driver.card = crh.readDriverCard();//"5050000830303031";// 
+//			read_count++;
+//		} while (read_count < 10 && current.driver.card == "");
 //
-//			String cmd;
-//			do{
-//				delay(1000);
-//				cmd = plch.readCommand();
-//			} while (!cmd.startsWith("010205"));//如果读到的PLC的指令不是以“010205”开头的，则一直等待
+//		if (current.driver.card != ""){
+//			current.time = millis();
 //
-//			//加水完成
-//			int kind;
-//			int potency;
-//			if (plch.getVolumnPotencyKind(cmd, &volumn, &potency, &kind)){
-//				//把相关的信息存储到服务器
-//				if (!simh.save(DEVICE_CODE, current.driver.code, current.trunk.code, volumn, potency, kind)){
-//					plch.saveError();
-//					//Serial.println("Save Error");
+//			//车卡和人卡都读出来了，把车牌号和人员编号发送给PLC
+//			current.trunk.code = crh.getPlate(current.trunk.card);
+//			current.driver.code = crh.getCode(current.driver.card);
+//			float volumn = crh.getVolumn(current.trunk.card);
+//
+//			//只有满足一下条件才能继续操作
+//			//1、读到的卡不一样
+//			//2、读到是特殊卡
+//			//3、同一张卡，超过了5分钟
+//			if (
+//				(current.driver.code + current.trunk.code == "0000E00000") ||
+//				(current.driver.code + current.trunk.code != last.driver.code + last.trunk.code) ||
+//				(current.time - last.time >= 300000)){
+//
+//				while (!plch.isReady()){
+//					delay(1000);//在PLC没有准备好的情况下，每1秒询问一次
+//					//Serial.println("PLC ready?");
 //				}
-//				else{
-//					plch.saveSuccess();
-//					//Serial.println("Save Success");
+//
+//				//Serial.println("PLC Ready");
+//				//PLC就绪
+//				plch.send(current.driver.code, current.trunk.code, volumn);
+//
+//				String cmd;// = "0102050051000000";
+//				do{
+//					delay(1000);
+//					cmd = plch.readCommand();
+//				} while (!cmd.startsWith("010205"));//如果读到的PLC的指令不是以“010205”开头的，则一直等待
+//
+//				//加水完成
+//				int kind;
+//				int potency;
+//				if (plch.getVolumnPotencyKind(cmd, &volumn, &potency, &kind)){
+//					/*Serial.print("plate = ");
+//					Serial.println(current.trunk.code);
+//					Serial.print("code = ");
+//					Serial.println(current.driver.code);
+//					Serial.print("volumn = ");
+//					Serial.println(volumn);
+//					Serial.print("potency = ");
+//					Serial.println(potency);
+//					Serial.print("kind = ");
+//					Serial.println(kind);*/
+//
+//					int saveCount = 0;
+//					int saved = 0;
+//
+//					do{
+//						openIP();
+//
+//						//把相关的信息存储到服务器，尝试提交5次
+//						saved = simh.save(DEVICE_CODE, current.driver.code, current.trunk.code, volumn, potency, kind);
+//						//Serial.print("Saved ");
+//						//Serial.println(saveCount);
+//						
+//						closeIP();
+//
+//						if (saved==Http_Save_Status_Success){
+//							break;
+//						}
+//					} while (saveCount++ < 5);
+//
+//					if (saved==Http_Save_Status_Success){
+//						plch.saveSuccess();
+//						//Serial.println("Save Success");
+//					}
+//					else{
+//						plch.saveError();
+//
+//						simh.sendShortMessage("NotSaved.Info:" + current.driver.code + "," + current.trunk.code + ".SaveStatus:" + saved + ".");
+//						//Serial.println("Save Error");
+//					}
 //				}
+//
+//				//完成一次加水过程，等到10秒
+//				delay(10000);
+//			
+//				last.driver.code = current.driver.code;
+//				last.trunk.code = current.trunk.code;
+//				last.time = current.time;
 //			}
-//
-//			//完成一次加水过程，等到30秒
-//			delay(30000);
-//
-//			last.driver.code = current.driver.code;
-//			last.trunk.code = current.trunk.code;
-//			last.time = current.time;
 //		}
 //	}
 //}
 
-
-void loop00() {
-	simh.sendShortMessage("test send message");
-	delay(10000);
-}
-
-//老读卡器（玖锐）
+//新读卡器（南京）
 void loop()
 {
 	/*
@@ -249,104 +289,77 @@ void loop()
 	5、返回步骤1
 	*/
 
-	shortMessage = "";
+	cards = crh.readCards();
+	if (cards != "") {
+		//delay(20);
+		current.trunk.card = cards.substring(0, cards.indexOf(","));//"544B000A4538323736320058";
+		current.driver.card = cards.substring(cards.indexOf(",") + 1);//"5050000830303031";
 
-	//delay(20);
-	current.trunk.card = crh.readTrunkCard();//"544B000A4538323736320058";
-	if (current.trunk.card != ""){
-		int read_count = 0;
+		//车卡和人卡都读出来了，把车牌号和人员编号发送给PLC
+		current.trunk.code = crh.getPlate(current.trunk.card);
+		current.driver.code = crh.getCode(current.driver.card);
+		float volumn = crh.getVolumn(current.trunk.card);
 
-		do{
-			//delay(20);
-
-			current.driver.card = crh.readDriverCard();//"5050000830303031";// 
-			read_count++;
-		} while (read_count < 10 && current.driver.card == "");
-
-		if (current.driver.card != ""){
-			current.time = millis();
-
-			//车卡和人卡都读出来了，把车牌号和人员编号发送给PLC
-			current.trunk.code = crh.getPlate(current.trunk.card);
-			current.driver.code = crh.getCode(current.driver.card);
-			float volumn = crh.getVolumn(current.trunk.card);
-
-			//只有满足一下条件才能继续操作
-			//1、读到的卡不一样
-			//2、读到是特殊卡
-			//3、同一张卡，超过了5分钟
-			if (
-				(current.driver.code + current.trunk.code == "0000E00000") ||
-				(current.driver.code + current.trunk.code != last.driver.code + last.trunk.code) ||
-				(current.time - last.time >= 300000)){
-
-				while (!plch.isReady()){
-					delay(1000);//在PLC没有准备好的情况下，每1秒询问一次
-					//Serial.println("PLC ready?");
-				}
-
-				//Serial.println("PLC Ready");
-				//PLC就绪
-				plch.send(current.driver.code, current.trunk.code, volumn);
-
-				String cmd;// = "0102050051000000";
-				do{
-					delay(1000);
-					cmd = plch.readCommand();
-				} while (!cmd.startsWith("010205"));//如果读到的PLC的指令不是以“010205”开头的，则一直等待
-
-				//加水完成
-				int kind;
-				int potency;
-				if (plch.getVolumnPotencyKind(cmd, &volumn, &potency, &kind)){
-					/*Serial.print("plate = ");
-					Serial.println(current.trunk.code);
-					Serial.print("code = ");
-					Serial.println(current.driver.code);
-					Serial.print("volumn = ");
-					Serial.println(volumn);
-					Serial.print("potency = ");
-					Serial.println(potency);
-					Serial.print("kind = ");
-					Serial.println(kind);*/
-
-					int saveCount = 0;
-					int saved = 0;
-
-					do{
-						openIP();
-
-						//把相关的信息存储到服务器，尝试提交5次
-						saved = simh.save(DEVICE_CODE, current.driver.code, current.trunk.code, volumn, potency, kind);
-						//Serial.print("Saved ");
-						//Serial.println(saveCount);
-						
-						closeIP();
-
-						if (saved==Http_Save_Status_Success){
-							break;
-						}
-					} while (saveCount++ < 5);
-
-					if (saved==Http_Save_Status_Success){
-						plch.saveSuccess();
-						//Serial.println("Save Success");
-					}
-					else{
-						plch.saveError();
-
-						simh.sendShortMessage("NotSaved.Info:" + current.driver.code + "," + current.trunk.code + ".SaveStatus:" + saved + ".");
-						//Serial.println("Save Error");
-					}
-				}
-
-				//完成一次加水过程，等到10秒
-				delay(10000);
+		//只有满足一下条件才能继续操作
+		//1、读到的卡不一样
+		//2、读到是特殊卡
+		//3、同一张卡，超过了5分钟
+		current.time = millis();
+		if (
+			(current.driver.code + current.trunk.code == "0000E00000") ||
+			(current.driver.code + current.trunk.code != last.driver.code + last.trunk.code) ||
+			(current.time - last.time >= 300000)) {
 			
-				last.driver.code = current.driver.code;
-				last.trunk.code = current.trunk.code;
-				last.time = current.time;
+			//在PLC没有准备好的情况下，每1秒询问一次
+			while (!plch.isReady()) {
+				delay(1000);
 			}
+
+			//PLC就绪
+			plch.send(current.driver.code, current.trunk.code, volumn);
+
+			String cmd;// = "0102050051000000";
+			do {
+				delay(1000);
+				cmd = plch.readCommand();
+			} while (!cmd.startsWith("010205"));//如果读到的PLC的指令不是以“010205”开头的，则一直等待
+
+			//加水完成
+			int kind;
+			int potency;
+			if (plch.getVolumnPotencyKind(cmd, &volumn, &potency, &kind)) {
+				int saveCount = 0;
+				int saved = 0;
+
+				do {
+					openIP();
+
+					//把相关的信息存储到服务器，尝试提交5次
+					saved = simh.save(DEVICE_CODE, current.driver.code, current.trunk.code, volumn, potency, kind);
+
+					closeIP();
+
+					if (saved == Http_Save_Status_Success) {
+						break;
+					}
+				} while (saveCount++ < 5);
+
+				if (saved == Http_Save_Status_Success) {
+					plch.saveSuccess();
+				}
+				else {
+					plch.saveError();
+
+					simh.sendShortMessage("NotSaved.Info:" + current.driver.code + "," + current.trunk.code + ".SaveStatus:" + saved + ".");
+				}
+			}
+
+			//完成一次加水过程，等到10秒
+			delay(10000);
+
+			last.driver.code = current.driver.code;
+			last.trunk.code = current.trunk.code;
+			last.time = current.time;
 		}
 	}
 }
